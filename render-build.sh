a7258c90e9f48ce0e3f8f78cd37e6df2cc793c1f
Ivan Martinez <ivanmartinezaguero@gmail.com>
2024-05-30 14:24:25 -0700

    closes if

--- render-build.sh
+++ render-build.sh
@@ -5,10 +5,10 @@ set -o errexit
 # Install Lighthouse globally
 npm install -g lighthouse
 
-
 STORAGE_DIR=/opt/render/project/.render
 
 if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo "...Downloading Chrome"
   mkdir -p $STORAGE_DIR/chrome
   cd $STORAGE_DIR/chrome
+fi
