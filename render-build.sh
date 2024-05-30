ad78903cc07f94d3f7ac0fe53fa39a3fb7bc0403
Ivan Martinez <ivanmartinezaguero@gmail.com>
2024-05-30 15:28:11 -0700

    render

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,4 @@
-#!/usr/bin/env bash
-# exit on error
 set -o errexit
-
-# Install Lighthouse globally
-npm install -g lighthouse
 
 STORAGE_DIR=/opt/render/project/.render
 
@@ -14,17 +9,7 @@ if [[ ! -d $STORAGE_DIR/chrome ]]; then
   wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
   rm ./google-chrome-stable_current_amd64.deb
-  chmod +x $STORAGE_DIR/chrome/opt/google/chrome/google-chrome
-  chmod +x $STORAGE_DIR/chrome/opt/google/chrome/chrome
-  chmod +x $STORAGE_DIR/chrome/opt/google/chrome/chrome-sandbox
-  export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
   cd $HOME/project/src # Make sure we return to where we were
 else
   echo "...Using Chrome from cache"
-  chmod +x $STORAGE_DIR/chrome/opt/google/chrome/google-chrome
-  chmod +x $STORAGE_DIR/chrome/opt/google/chrome/chrome
-  chmod +x $STORAGE_DIR/chrome/opt/google/chrome/chrome-sandbox
 fi
-
-# Debug: List contents of the Chrome directory
-ls -la $STORAGE_DIR/chrome/opt/google/chrome
